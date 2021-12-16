c693cdd463728dab4bbfc512c311c65bfa2c4295
Salompablo <pablosalompita@gmail.com>
2021-12-16 14:45:19 +0100

    Create Iteración3

---
+++ Iteración3.js
@@ -0,0 +1,48 @@
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+let favSounds = (data) => {
+
+let repetidos = {}
+for (let usuario of users){
+    for (let sound in usuario.favoritesSounds){
+        if (repetidos[sound] === undefined) {
+            repetidos[sound] = 1
+        } else {
+            repetidos[sound] += 1
+        }
+    }
+}
+return repetidos
+}
+
+let objRepetidos = favSounds(users)
+console.log(objRepetidos)
