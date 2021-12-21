fd9c48a0ff2f23710b498ae8f2967c41abe1b286
Salompablo <pablosalompita@gmail.com>
2021-12-21 13:09:20 +0100

    Create Iteración4

---
+++ Iteración4.js
@@ -0,0 +1,13 @@
+// Crea una función llamada findArrayIndex que reciba como parametros un array de textos y un texto y devuelve la posición del
+// array cuando el valor del array sea igual al valor del texto que enviaste como parametro. Haz varios ejemplos y compruebalos.
+const array1 = ['Caracol', 'Mosquito', 'Salamandra', 'Ajolote']
+
+function findArrayIndex(array, text) {
+    let index = array.indexOf(text)
+    return index
+}
+
+console.log(findArrayIndex(array1, "Ajolote"))
+
+
+
